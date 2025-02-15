23887933a028ce7afdbaf047462e53acc9bdeb82
eexanem <eexanem@gmail.com>
2025-02-15 11:05:44 +0000

    chore: save initial Anchor project setup

--- programs/betting-program/src/lib.rs
+++ programs/betting-program/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Bpeo86v5xF7jBoSD5MheMDeppmfEQKRSRgYoCXmoWvRp");
+declare_id!("3VCmwMXEsnak5A58TYDfrtNWd8crioEhkytCmAr91ZsG");
 
 #[program]
 pub mod betting_program {
